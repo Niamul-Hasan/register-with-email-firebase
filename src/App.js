e0492e7aded39f66f4421dc2c7c338afd52db0f8
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-04-08 20:44:20 +0600

    log in form created by firebase authentication

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from 'react';
 
 
@@ -10,10 +10,15 @@ const auth = getAuth(app);
 
 function App() {
   const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [validated, setValidated] = useState(false);
   const [error, setError] = useState('');
   const [registered, setRegistered] = useState(false);
+
+  const handleUserNameBlur = event => {
+    setName(event.target.value);
+  }
   const handleEmailBlur = event => {
     setEmail(event.target.value);
   }
@@ -22,6 +27,29 @@ function App() {
   }
   const handleRegisteredCheck = event => {
     setRegistered(event.target.checked);
+  }
+
+  const updateUser = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name
+    })
+      .then(() => {
+        console.log('user is updating')
+      })
+  }
+
+  const getVarification = () => {
+    sendEmailVerification(auth.currentUser)
+      .then(() => {
+        console.log("verification email has sent");
+      })
+  }
+
+  const passwordReset = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("password Reset email has sent");
+      })
   }
 
   const handleSubmit = event => {
@@ -49,17 +77,21 @@ function App() {
           console.error(error);
           setError(error.message);
         })
+
     }
     else {
       createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
           const user = result.user;
           console.log(user);
+          getVarification();
+          updateUser();
         })
         .catch(error => {
           console.error(error);
           setError(error.message);
-        })
+        });
+
     }
     event.preventDefault();
   }
@@ -68,6 +100,13 @@ function App() {
       <div className="registration w-50 mx-auto mt-2">
         <h2 className="text-primary">Please {registered ? "Log In" : "Register First"}</h2>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>User Name</Form.Label>
+            <Form.Control onBlur={handleUserNameBlur} type="text" placeholder="Enter Your Name" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide Your Name.
+            </Form.Control.Feedback>
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
@@ -93,6 +132,7 @@ function App() {
           <Button variant="primary" type="submit">
             {registered ? "Log In" : "Register"}
           </Button>
+          <Button onClick={passwordReset} variant="link">Forget Password?</Button>
         </Form>
       </div>
 
